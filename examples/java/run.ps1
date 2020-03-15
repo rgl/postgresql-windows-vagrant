e0415fa4ed0e9b0c7fd8bfa2842613933a212d5c
Rui Lopes <rgl@ruilopes.com>
2020-03-15 12:15:14 +0000

    upgrade to java 11

--- examples/java/run.ps1
+++ examples/java/run.ps1
@@ -1,5 +1,6 @@
 # install dependencies.
-choco install -y adoptopenjdk8 gradle
+choco install -y adoptopenjdk11
+choco install -y gradle --version 6.2.2
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
@@ -9,7 +10,7 @@ Update-SessionEnvironment
 # NB gradle build would also work, but having a fat jar is nicier for distribution.
 Write-Output 'Building the example...'
 $env:GRADLE_OPTS = '-Dorg.gradle.daemon=false' # to save memory, do not leave the daemon running in background.
-gradle clean shadowJar
+gradle clean shadowJar --warning-mode all
 
 # run the example.
 Write-Output 'Executing the example...'
