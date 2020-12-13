09ecc6f508583bf578629afbc40b8b7c6955a6e0
Rui Lopes <rgl@ruilopes.com>
2020-12-13 19:24:29 +0000

    upgrade to .net core sdk 3.1.404 (includes .net core 3.1.10)

--- examples/csharp/run.ps1
+++ examples/csharp/run.ps1
@@ -1,14 +1,15 @@
 if (!(Get-Command -ErrorAction SilentlyContinue dotnet.exe)) {
     # see https://dotnet.microsoft.com/download/dotnet-core/3.1
-    # see https://github.com/dotnet/core/blob/master/release-notes/3.1/3.1.2/3.1.2.md
+    # see https://github.com/dotnet/core/blob/master/release-notes/3.1/3.1.10/3.1.404-download.md
 
     # opt-out from dotnet telemetry.
     [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
     $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
     # install the dotnet sdk.
-    $archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/5aad9c2c-7bb6-45b1-97e7-98f12cb5b63b/6f6d7944c81b043bdb9a7241529a5504/dotnet-sdk-3.1.102-win-x64.exe'
-    $archiveHash = '361c805703812e1463e7660de99e569f476ea9164fd4267f314ce10b865321ba44d5afc3eaaed7c8617f7787845d08bcfbc2d3914441a16d47c87c88a1404390'
+    # NB keep this in sync with provision-iis-dotnetcore-hosting.ps1
+    $archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/3366b2e6-ed46-48ae-bf7b-f5804f6ee4c9/186f681ff967b509c6c9ad31d3d343da/dotnet-sdk-3.1.404-win-x64.exe'
+    $archiveHash = '9d12890913ec5e0d6894f769bb704673a8e15619ef19d0d1906a19f80be53f6ccbbf3f451e069676a1512755e1cd4fc4ee36becb9aa67e3c8edbce76fc854ddf'
     $archiveName = Split-Path -Leaf $archiveUrl
     $archivePath = "$env:TEMP\$archiveName"
     Write-Host "Downloading $archiveName..."
