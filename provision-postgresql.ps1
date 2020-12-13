07d673565b982376efa28aae01d95136540d0c6f
Rui Lopes <rgl@ruilopes.com>
2020-12-13 19:27:59 +0000

    upgrade to postgresql 13.1

--- provision-postgresql.ps1
+++ provision-postgresql.ps1
@@ -6,14 +6,14 @@ Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 #    postgres by running:
 #       psql -c 'select version()' postgres
 #    which returns something like:
-#       PostgreSQL 11.0, compiled by Visual C++ build 1914, 64-bit
+#       PostgreSQL 13.1, compiled by Visual C++ build 1914, 64-bit
 #    that build 1914 is for:
 #       MSVC++ 14.14 _MSC_VER == 1914 (Visual Studio 2017 version 15.7).
 #    see https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B
 choco install -y vcredist2017
 
 # the default postgres superuser username and password.
-# see https://www.postgresql.org/docs/11/libpq-envars.html
+# see https://www.postgresql.org/docs/13/libpq-envars.html
 $env:PGUSER = 'postgres'
 $env:PGPASSWORD = 'postgres'
 
@@ -45,8 +45,8 @@ function psql {
 
 # download and install binaries.
 # see https://www.enterprisedb.com/download-postgresql-binaries
-$archiveUrl = 'https://get.enterprisedb.com/postgresql/postgresql-12.2-1-windows-x64-binaries.zip'
-$archiveHash = '6000cb43e17bfcaea4ec36dc115804285cd3a52d7d9bff2f385da3d43f385fdb'
+$archiveUrl = 'https://get.enterprisedb.com/postgresql/postgresql-13.1-1-windows-x64-binaries.zip'
+$archiveHash = '90f80410bbfda295741b09c3b7ae2e9f35981fbfabdabc1ca876e1c408ddf283'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Output "Downloading from $archiveUrl..."
@@ -61,15 +61,7 @@ Move-Item "$serviceHome\pgsql\*" $serviceHome
 rmdir "$serviceHome\pgsql"
 Remove-Item $archivePath
 
-# postgresql 11.0 was failing with "exit code -1073741515" error because
-# libwinpthread-1.dll was not found, this will copy it from git.
-# TODO report this error.
-# if (!(Test-Path "$serviceHome\bin\libwinpthread-1.dll")) {
-#     Write-Output "Copying missing libwinpthread-1.dll..."
-#     Copy-Item 'C:\Program Files\Git\mingw64\bin\libwinpthread-1.dll' "$serviceHome\bin"
-# }
-
-# see https://www.postgresql.org/docs/11/event-log-registration.html
+# see https://www.postgresql.org/docs/13/event-log-registration.html
 # see the available log names with:
 #       Get-WinEvent -ListLog * | Sort-Object LogName | Format-Table LogName
 # see the providers that write to a specific log with:
@@ -136,7 +128,7 @@ host    all             all             ::/0                    md5
 '@ `
     | Out-File -Append -Encoding ascii "$dataPath\pg_hba.conf"
 
-# see https://www.postgresql.org/docs/11/libpq-ssl.html
+# see https://www.postgresql.org/docs/13/libpq-ssl.html
 Write-Host 'Enabling ssl...'
 mkdir -Force "$env:APPDATA/postgresql" | Out-Null
 Copy-Item c:/vagrant/shared/postgresql-example-ca/postgresql-example-ca-crt.pem "$env:APPDATA/postgresql/root.crt"
@@ -171,7 +163,7 @@ Write-Output 'Installing the adminpack extension...'
 psql -c 'create extension adminpack' postgres
 
 Write-Output 'Showing pg version, connection information, users and databases...'
-# see https://www.postgresql.org/docs/11/functions-info.html
+# see https://www.postgresql.org/docs/13/functions-info.html
 psql -c 'select version()' postgres
 psql -c 'select current_user, current_database(), inet_client_addr(), inet_client_port(), inet_server_addr(), inet_server_port(), pg_backend_pid(), pg_postmaster_start_time()' postgres
 psql -c '\du' postgres
