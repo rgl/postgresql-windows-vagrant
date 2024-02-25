57d019853467c8483708d72f97367cf733d76d2c
Rui Lopes <rgl@ruilopes.com>
2024-02-25 16:38:59 +0000

    upgrade to pgadmin4 8.2.0

--- provision-pgadmin.ps1
+++ provision-pgadmin.ps1
@@ -1,5 +1,5 @@
 # install.
 # see https://community.chocolatey.org/packages/pgadmin4
 # renovate: datasource=nuget:chocolatey depName=pgadmin4
-$pgadmin4Version = '8.0.0'
+$pgadmin4Version = '8.2.0'
 choco install -y pgadmin4 --version $pgadmin4Version
